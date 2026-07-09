9d1b738f1df18ddf6a791e3c8114c2d5fb630851
Napapat33 <164392@wsk.ac.th>
2026-07-09 15:24:20 +0700

    Update program01.py

--- program01.py
+++ program01.py
@@ -1,5 +1,21 @@
-print("โปรแกรมคำนวณหาพื้นที่วงกลม")
-r=float(input("enter r:"))
-area=3.14*r*r
-print(f"area is:{area:.2f}")
+print("Area Calculator")
 
+print("1. Rectangle")
+print("2. Triangle")
+
+choice = int(input("Select a menu: "))
+
+if choice == 1:
+    width = float(input("Width: "))
+    length = float(input("Length: "))
+    area = width * length
+    print("Area =", area)
+
+elif choice == 2:
+    base = float(input("Base length: "))
+    height = float(input("Height: "))
+    area = 0.5 * base * height
+    print("Area =", area)
+
+else:
+    print("Invalid menu selection")
